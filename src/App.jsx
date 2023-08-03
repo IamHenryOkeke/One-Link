d5ffd38973f94cc98a73c9ae606666b62e37ead6
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-03 13:32:26 +0100

    import for component and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import ContactCard from "./views/ContactMe"
+import Footer from "./components/Footer/Footer"
+import NavBar from "./components/NavBar/NavBar"
+// import Projects from "./views/Projects"
+// import AboutMe from "./views/AboutMe"
+// import Home from "./views/Home"
 
+
+const App = () => {
   return (
-    <h1 className="text-6xl font-bold text-red-500">Hello</h1>
+    <Router>
+      <div className="bg-[#130b0f] font-primaryFont text-[#f0e5ea] pt-20">
+          <NavBar/>
+          <Routes>
+            {/* <Route exact path='/' element={<Home/>} />
+            <Route exact path='/about' element={<AboutMe/>}/>
+            <Route exact path='/projects' element={<Projects/>} />           
+            <Route exact path='/contact-me' element={<ContactCard twitterUrl="https://twitter.com/NmesomaHenry" portfolioUrl='https://github.com/IamHenryOkeke' phone='+2347080194374' linkedInUrl='http://www.linkedin.com/in/henry-okeke-3176481a9' emailAddress="iamhenryokeke@gmail.com"/> } /> */}
+          </Routes>
+          <Footer/>
+      </div>
+    </Router>
   )
 }
 
 export default App
+
+
