269ab095ceadab4c57fad9119c392a51536dbec0
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-03 19:49:21 +0100

    finished navbar and navbar animation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,16 +2,17 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as MenuClose } from '../../assets/svg/menuClose.svg'
 import { ReactComponent as MenuOpen } from '../../assets/svg/menuOpen.svg'
 import { useState } from "react"
+import { Transition } from '@headlessui/react'
 
 const NavBar = () => {
-  const [openNav, setOpenNav] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
   const handleNavOpen = () => {
-    setOpenNav(true)
+    setShowNav(true)
   }
 
   const handleNavClose = () => {
-    setOpenNav(false)
+    setShowNav(false)
   }
   
   return (
@@ -26,23 +27,28 @@ const NavBar = () => {
           
         </div>
         <ul className='hidden md:flex gap-8'>
-          <li className="tracking-normal transition-all ease-in-out delay-150 hover:text-blue-500 hover:tracking-widest duration-300 cursor-pointer"><Link to="/home">Home</Link> </li>
-          <li className="tracking-normal transition-all ease-in-out delay-150 hover:text-blue-500 hover:tracking-widest duration-300 cursor-pointer"><Link to="/about">Links</Link> </li>
-          <li className="tracking-normal transition-all ease-in-out delay-150 hover:text-blue-500 hover:tracking-widest duration-300 cursor-pointer"><Link to="/projects">Profile</Link> </li>
+          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/home">Home</Link> </li>
+          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/links">Links</Link> </li>
+          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/profile">Profile</Link> </li>
         </ul>
         <div className='md:hidden'>
             <MenuOpen  onClick = {() => handleNavOpen()} className="fill-white h-6 w-6"/>
         </div>
-        <div className={`${openNav ? "absolute" : "hidden"} z-50 top-0 pt-[50%] bg-slate-900 h-[100vh] opacity-90 w-[100vw]`}>
-          <MenuClose onClick = {() => handleNavClose()} className="fill-white h-10 w-10 absolute top-10 left-5"/>
-          <ul className='flex flex-col items-center gap-8 text-3xl text-white'>
-            <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/home">Home</Link> </li>
-            <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/about">Manage Links</Link> </li>
-            <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/projects">Profile</Link> </li>
-          </ul>
+          <Transition
+            show={showNav}
+            className={`${showNav ? "absolute" : "hidden"} z-50 top-0 bg-slate-900  pt-[50%] h-[100vh] opacity-90 w-[100vw]`}
+            enter="transform transition ease-in-out duration-500"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+          >
+            <MenuClose onClick = {() => handleNavClose()} className="fill-white h-10 w-10 absolute top-10 left-5"/>
+            <ul className='flex flex-col items-center gap-8 text-3xl text-white'>
+              <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/home">Home</Link> </li>
+              <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/links">Manage Links</Link> </li>
+              <li onClick={() => handleNavClose()} className="cursor-pointer"><Link to="/profile">Profile</Link> </li>
+            </ul>
+          </Transition>
         </div>
-      </div>
-      
     </nav>
   )
 }
