da547679974c9d93ec32208f31094e5bcdad3732
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-03 19:47:26 +0100

    added custom animation for buy coffee svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ export default {
         primaryFont : ['Montserrat', 'sans-serif'],
         secondaryFont :['Dancing Script', "cursive"],
         nameFont : ['Victor Mono', "monospace"]
+      },
+      keyframes : {
+        wiggle:{
+          '0%, 100%, 60%': { transform: 'rotate(0) scale(1)'},
+          '75%': { transform: 'rotate(0) scale(1.12)' },
+          '80%': { transform: 'rotate(0) scale(1.1)' },
+          '84%': { transform: 'rotate(-10deg) scale(1.1)' },
+          '88%': { transform: 'rotate(10deg) scale(1.1)' },
+          '92%': { transform: 'rotate(-10deg) scale(1.1)' },
+          '96%': { transform: 'rotate(10deg) scale(1.1)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 3s infinite',
       }
     },
   },
