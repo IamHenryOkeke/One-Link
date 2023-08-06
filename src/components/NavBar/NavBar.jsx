49f6a775ae9aa16e6fcbd1a22b2c83149c399a06
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-06 21:47:54 +0100

    conditional rendering for navbar links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { ReactComponent as MenuClose } from '../../assets/svg/menuClose.svg'
 import { ReactComponent as MenuOpen } from '../../assets/svg/menuOpen.svg'
 import { useState } from "react"
 import { Transition } from '@headlessui/react'
 
 const NavBar = () => {
+  const location = useLocation();
+  console.log(location)
   const [showNav, setShowNav] = useState(false);
 
   const handleClick = () => {
@@ -33,17 +35,24 @@ const NavBar = () => {
              OneLink©
             </p>
           </a>
-          
         </div>
-        <ul className='hidden md:flex gap-8'>
-          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/home">Home</Link> </li>
-          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/links">Links</Link> </li>
-          <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/profile">Profile</Link> </li>
-        </ul>
+        {(location.pathname === "/") ? (
+          <div className='hidden md:flex gap-8'>
+            <button>Log in </button>
+            <button>Sign up</button>
+          </div>
+        ) : (
+          <ul className='hidden md:flex gap-8'>
+            <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/home">Home</Link> </li>
+            <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/links">Links</Link> </li>
+            <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/profile">Profile</Link> </li>
+          </ul>
+        )}
+        
         <div className='md:hidden'>
-            <MenuOpen  onClick = {() => handleClick()} className="fill-white h-6 w-6"/>
+            <MenuOpen onClick = {() => handleClick()} className="fill-white h-6 w-6"/>
         </div>
-          <Transition.Root
+          <Transition
             show={showNav}
             className="absolute top-0 bg-slate-900 pt-[50%] h-[100vh] opacity-90 w-[100vw]"
             enter="transform transition ease-in-out duration-500"
@@ -54,18 +63,25 @@ const NavBar = () => {
             leaveTo="translate-x-full"
           >
             <MenuClose onClick = {() => handleClick()} className="fill-white h-10 w-10 absolute top-10 left-5"/>
-            <ul className='flex flex-col items-center gap-8 text-3xl text-white'>
-              <FadeIn delay="delay-[200ms]">
-                <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/home">Home</Link> </li>
-              </FadeIn>
-              <FadeIn delay="delay-[400ms]">
-                <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/links">Manage Links</Link> </li>
-              </FadeIn>
-              <FadeIn delay="delay-[800ms]">
-                <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/profile">Profile</Link> </li>
-              </FadeIn>
-            </ul>
-          </Transition.Root>
+            {(location.pathname === "/") ? (
+              <div className='flex flex-col items-center gap-8'>
+                <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Log in </button>
+                <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign up</button>
+              </div>
+            ) : (
+              <ul className='flex flex-col items-center gap-8 text-3xl text-white'>
+                <FadeIn delay="delay-[200ms]">
+                  <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/home">Home</Link> </li>
+                </FadeIn>
+                <FadeIn delay="delay-[400ms]">
+                  <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/links">Manage Links</Link> </li>
+                </FadeIn>
+                <FadeIn delay="delay-[800ms]">
+                  <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/profile">Profile</Link> </li>
+                </FadeIn>
+              </ul>
+            )}
+          </Transition>
         </div>
     </nav>
   )
