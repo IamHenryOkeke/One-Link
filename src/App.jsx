e076df6df407a207092c74fb8cfba61851f70c90
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-12 20:50:58 +0100

    views import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import ContactCard from "./views/ContactMe"
 import Footer from "./components/Footer/Footer"
 import NavBar from "./components/NavBar/NavBar"
-// import Projects from "./views/Projects"
-// import AboutMe from "./views/AboutMe"
-// import Home from "./views/Home"
-
+import LogIn from "./views/LogIn"
+import SignUp from "./views/SignUp"
+import LandingPage from "./views/LandingPage"
+import Home from "./views/Home"
+import ManageLinks from "./views/ManageLinks"
 
 const App = () => {
   return (
@@ -13,10 +13,11 @@ const App = () => {
       <div className="bg-[#130b0f] font-primaryFont text-[#f0e5ea] pt-20">
           <NavBar/>
           <Routes>
-            {/* <Route exact path='/' element={<Home/>} />
-            <Route exact path='/about' element={<AboutMe/>}/>
-            <Route exact path='/projects' element={<Projects/>} />           
-            <Route exact path='/contact-me' element={<ContactCard twitterUrl="https://twitter.com/NmesomaHenry" portfolioUrl='https://github.com/IamHenryOkeke' phone='+2347080194374' linkedInUrl='http://www.linkedin.com/in/henry-okeke-3176481a9' emailAddress="iamhenryokeke@gmail.com"/> } /> */}
+            <Route exact path='/' element={<LandingPage/>}/>
+            <Route exact path='/login' element={<LogIn/>}/>
+            <Route exact path='/signup' element={<SignUp/>}/>
+            <Route exact path='/profile' element={<Home/>}/> 
+            <Route exact path='/links' element={<ManageLinks/>}/> 
           </Routes>
           <Footer/>
       </div>
