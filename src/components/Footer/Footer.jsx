1fd9b3ec61dae0b8f63fd3b487426b797f3a7539
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-15 16:43:07 +0100

    ui fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import {ReactComponent as Icon} from "../../assets/svg/bmc-logo.svg"
 const Footer = () => {
   const date = new Date
   return (
-    <footer className="text-center mt-5 text-sm md:text-base py-6 bg-black flex flex-col items-center gap-6">
+    <footer className="text-center mt-5 text-sm md:text-base py-6 flex flex-col items-center gap-6">
       <div className = "flex items-center justify-center gap-2 bg-white w-fit text-black py-3 px-5 rounded-lg font-bold">
-         <p>Buy us coffee</p>
+         <p>Buy me coffee</p>
          <a href=""  className="animate-wiggle">
           <Icon/>
          </a>
@@ -13,7 +13,7 @@ const Footer = () => {
       </div>
      
       <p>
-         © {date.getFullYear()} NmesomaHenry All Rights Reserved.
+         © {date.getFullYear()} <a className="hover:drop-shadow-[0_35px_35px_rgba(83,185,65,1)]" href="http://github.com/Iamhenryokeke" target="_blank" rel="noreferrer">NmesomaHenry</a>  All Rights Reserved.
       </p>
     </footer>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,14 +3,10 @@ import { ReactComponent as MenuClose } from '../../assets/svg/menuClose.svg'
 import { ReactComponent as MenuOpen } from '../../assets/svg/menuOpen.svg'
 import { useState } from "react"
 import { Transition } from '@headlessui/react'
-import useUserStatus from '../../utils/useUserStatus'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../firebase'
 
-
-
 const NavBar = () => {
-  const user =  useUserStatus();
   const location = useLocation();
   const navigate = useNavigate()
   const [showNav, setShowNav] = useState(false);
@@ -43,11 +39,11 @@ const NavBar = () => {
   )
   
   return (
-    <nav className='fixed z-[50] bg-[#aca4a4] text-black top-0 py-5 px-4 md:px-6 lg:px-60 w-full'>
+    <nav className='fixed z-[50] bg-[#070308] text-[#f3e5f6] top-0 py-5 px-4 md:px-6 lg:px-60 w-full'>
       <div className='flex justify-between items-center'>
         <div>
           <a href="/">
-            <p className='font-bold text-white font-secondaryFont text-2xl'>
+            <p className='font-secondaryFont font-extrabold text-2xl'>
              OneLink©
             </p>
           </a>
@@ -55,29 +51,29 @@ const NavBar = () => {
         {(location.pathname === "/") ? (
           <div className='hidden md:flex gap-8'>
             <Link to="/login">
-              <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Log in</button>
+              <button className='transition-all ease-in-out delay-150 px-5 py-3 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Log in</button>
             </Link>
             <Link to="/signup">
-              <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign up</button>
+              <button className='transition-all ease-in-out delay-150 px-5 py-3 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign up</button>
             </Link>
           </div>
         ) : (location.pathname === "/login") ? (
                 <Link to="/signup">
-                  <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign up</button>
+                  <button className='transition-all ease-in-out delay-150 px-5 py-3 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign up</button>
                 </Link>
         ) : (location.pathname === "/signup") ? (
                 <Link to="/login">
-                  <button className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Log in</button>
+                  <button className='transition-all ease-in-out delay-150 px-5 py-3 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Log in</button>
                 </Link>
                
         ) : (
           <div className='hidden md:flex gap-8 items-center'>
             <ul className='hidden md:flex gap-8'>
-              <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/profile">Profile</Link> </li>
-              <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/links">Manage Links</Link> </li>
-              <li className="transition-all ease-in-out delay-150 duration-300 cursor-pointer"><Link to="/settings">Settings</Link> </li>
+              <li className="font-bold transition-all ease-in-out delay-150 hover:text-[#53b941] hover:tracking-widest duration-300 cursor-pointer"><Link to="/home">Profile</Link> </li>
+              <li className="font-bold transition-all ease-in-out delay-150 hover:text-[#53b941] hover:tracking-widest duration-300 cursor-pointer"><Link to="/manage-links">Manage Links</Link></li>
+              <li className="font-bold transition-all ease-in-out delay-150 hover:text-[#53b941] hover:tracking-widest duration-300 cursor-pointer"><Link to="/settings">Settings</Link> </li>
             </ul>
-            <button onClick={handleSignOut} className='px-4 py-1 bg-blue-600 rounded-md font-bold text-white'>Sign Out</button>
+            <button onClick={handleSignOut} className='transition-all ease-in-out delay-150 px-4 py-2 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign Out</button>
           </div>
           
         )}
@@ -99,36 +95,36 @@ const NavBar = () => {
             {(location.pathname === "/") ? (
                   <div className='flex flex-col items-center gap-8'>
                     <Link to="/login">
-                      <button onClick={() => handleClick()} className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Log in</button>
+                      <button onClick={() => handleClick()} className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Log in</button>
                     </Link>
                     <Link to="/signup">
-                      <button onClick={() => handleClick()} className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign up</button>
+                      <button onClick={() => handleClick()} className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign up</button>
                     </Link>
                   </div>
                 ) : (location.pathname === "/login") ? (
                         <Link className='flex flex-col items-center' to="/signup">
-                          <button onClick={() => handleClick()} className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign up</button>
+                          <button onClick={() => handleClick()} className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign up</button>
                         </Link>
                 ) : (location.pathname === "/signup") ? (
                         <Link className='flex flex-col items-center' to="/login">
-                          <button onClick={() => handleClick()} className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Log in</button>
+                          <button onClick={() => handleClick()} className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Log in</button>
                         </Link>
                       
                 ) : (
                   <div className='flex flex-col items-center gap-8 text-3xl text-white'>
                     <ul className='flex flex-col items-center gap-8'>
                       <FadeIn delay="delay-[150ms]">
-                        <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/profile">Profile</Link></li>
+                        <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/home">Profile</Link></li>
                       </FadeIn>
                       <FadeIn delay="delay-[300ms]">
-                        <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/links">Manage Links</Link></li>
+                        <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/manage-links">Manage Links</Link></li>
                       </FadeIn>
                       <FadeIn delay="delay-[450ms]">
                         <li onClick={() => handleClick()} className="cursor-pointer"><Link to="/settings">Settings</Link></li>
                       </FadeIn>
                     </ul>
                     <FadeIn delay="delay-[600ms]">
-                      <button onClick={handleSignOut} className='px-5 py-3 bg-blue-600 rounded-md font-bold text-white text-lg'>Sign Out</button>
+                      <button onClick={handleSignOut} className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#0c221f] hover:shadow-lg hover:shadow-[#0c221f] lg:self-start rounded-md font-bold text-white text-lg duration-300'>Sign Out</button>
                     </FadeIn>
                   </div>
               
