1e59c40e1513df475d9f2785202be9d65c76af9f
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-15 16:40:32 +0100

    nested rooting for home view

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import NavBar from "./components/NavBar/NavBar"
 import LogIn from "./views/LogIn"
 import SignUp from "./views/SignUp"
 import LandingPage from "./views/LandingPage"
-import Home from "./views/Home"
+import Home from "./views/Home/Home"
 import ManageLinks from "./views/ManageLinks"
+import AdminLinks from "./views/Home/AdminLinks"
+import UserLinks from "./views/Home/UserLinks"
+
 
 const App = () => {
   return (
     <Router>
-      <div className="bg-[#130b0f] font-primaryFont text-[#f0e5ea] pt-20">
+      <div className="bg-[#070308] font-primaryFont text-[#f3e5f6] pt-20">
           <NavBar/>
           <Routes>
             <Route exact path='/' element={<LandingPage/>}/>
             <Route exact path='/login' element={<LogIn/>}/>
             <Route exact path='/signup' element={<SignUp/>}/>
-            <Route exact path='/profile' element={<Home/>}/> 
-            <Route exact path='/links' element={<ManageLinks/>}/> 
+            <Route exact path='/home' element={<Home/>}>
+              <Route exact path="admin-links" element = {<AdminLinks/>}/>
+              <Route exact path="user-links" element = {<UserLinks/>}/>
+            </Route> 
+            <Route exact path='/manage-links' element={<ManageLinks/>}/> 
           </Routes>
           <Footer/>
       </div>
