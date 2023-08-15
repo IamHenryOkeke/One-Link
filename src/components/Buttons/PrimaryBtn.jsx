3bf8438866cee4a76c26ff8f701ebf3fd85d7317
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-15 16:42:30 +0100

    created primary and secondary for project

---
+++ src/components/Buttons/PrimaryBtn.jsx
@@ -0,0 +1,7 @@
+const PrimaryBtn = ({text}) => {
+    return (
+        <button className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#429334] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#429334] lg:self-start rounded-md font-bold text-white text-lg duration-300'>{text}</button>
+    )
+}
+
+export default PrimaryBtn
---
+++ src/components/Buttons/SecondaryBtn.jsx
@@ -0,0 +1,7 @@
+const SecondaryBtn = ({text}) => {
+    return (
+        <button className='transition-all ease-in-out delay-150 px-5 py-4 bg-[#0c221f] hover:-translate-y-1 hover:shadow-lg hover:shadow-[#0c221f] lg:self-start rounded-md font-bold text-white text-lg duration-300'>{text}</button>
+    )
+}
+
+export default SecondaryBtn
