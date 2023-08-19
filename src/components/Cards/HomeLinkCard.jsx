d7ed59efe5af30935a8956487e117166a7c5b96b
Henry Okeke <iamhenryokeke@gmail.com>
2023-08-19 13:22:44 +0100

    fixed whitespace issue and ui fix for user links page

--- src/components/Cards/HomeLinkCard.jsx
+++ src/components/Cards/HomeLinkCard.jsx
@@ -1,7 +1,7 @@
 const HomeLinkCard = ({title, link}) => {
     return (
-        <a className="bg-slate-700 cursor-pointer p-5 rounded-lg w-[90%] md:w-[60%] lg:w-[40%] flex items-center justify-between" href={link} target="_blank" rel="noreferrer">
-            <span className="no-underline hover:underline capitalize font-bold text-sm md:text-lg">
+        <a className="bg-slate-700 cursor-pointer p-5 rounded-lg flex items-center justify-between w-[90%] md:w-[60%]" href={link} target="_blank" rel="noreferrer">
+            <span className="no-underline hover:underline capitalize font-bold text-md md:text-lg">
                 {title}
             </span>
             <svg className="w-7 h-7" data-v-47436778="" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1.5" strokeLinecap="square" strokeLinejoin="arcs" role="presentation" focusable="false" tabIndex="-1"><path data-v-47436778="" d="M7 17l9.2-9.2M17 17V7H7"></path></svg>
--- src/views/Home/UserLinks.jsx
+++ src/views/Home/UserLinks.jsx
@@ -6,24 +6,23 @@ import HomeLinkCard from "../../components/Cards/HomeLinkCard"
 const UserLinks = () => {
     const [userLinks, setUserLinks] = useState([])
     const [paraText, setParaText] = useState("")
+    const [userName, setUserName] = useState("")
     const [userLoading, setUserLoading] = useState(false)
     const [value, setValue] = useState("")
-    // const paraElement = useRef();
 
     const handleOnFetch = async() => {
         setUserLoading(true)
         setParaText("")
+        setUserName("")
         try {
-            const response = await getDocs(collection(userDataBase, value.toLowerCase()))
+            const response = await getDocs(collection(userDataBase, value.toLowerCase().trim()))
             let links = [];
             response.docs.forEach((doc) => {
             links.push({
                     ...doc.data(), id: doc.id
                 })
-            })
-            if(links.length === 0){
-                setParaText("No links found")
-            }
+            });
+            (links.length === 0) ? setParaText("No links found") : setUserName(value)
             setUserLoading(false)
             setUserLinks(links)
         } catch (error) {
@@ -35,18 +34,22 @@ const UserLinks = () => {
 
     return (
         <div className="mx-4">
-            <div className="flex flex-col items-center md:flex-row md:justify-center gap-4 md:text-xl mt-5">
+            <div className="flex flex-col items-center lg:flex-row md:justify-center gap-4 md:text-xl mt-5">
                 <input className='text-black  px-4 py-2 md:py-4 rounded-md w-[80%] lg:w-[50%]' type="url"  name="link" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Enter username" required/>
                 <button onClick={handleOnFetch}  className="px-8 py-2 md:py-4 self-center bg-[#aca4a4] rounded-md text-white">Fetch links</button>
             </div>
             <div className="flex flex-col items-center mt-4">
                 {userLoading && <p>Fetching user links...</p>}
                 <p>{paraText}</p>
-                {
-                    (userLinks.map((link) => (
-                        <HomeLinkCard key={link.id} {...link}/>
-                    )))
-                }
+                <div className="flex flex-col gap-4 items-center w-full mt-4">
+                    <h1 className="text-lg md:text-2xl">{userName}</h1>
+                    {
+                        (userLinks.map((link) => (
+                            <HomeLinkCard key={link.id} {...link}/>
+                        )))
+                    }
+                </div>
+                
             </div>
              
             
